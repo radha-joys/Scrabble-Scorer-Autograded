8d3a3f207c3cc67439754af18a2b42dc29164dd4
Radha Sri Vepuri <143245819+radha-joys@users.noreply.github.com>
2024-02-03 16:14:03 +0000

    Update

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -111,6 +111,7 @@ function runProgram() {
    scorerPrompt();
 }
 
+
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
